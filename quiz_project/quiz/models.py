c00a79822d642fed4f2164c3d9fc7ea6ee8a70ba
yasserelsayed190@gmail.com <yasserelsayed190@gmail.com>
2022-04-08 19:17:32 +0500

    fix home screen

--- quiz_project/quiz/models.py
+++ quiz_project/quiz/models.py
@@ -8,7 +8,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    avatar = models.ImageField(default='default.png', upload_to='profile_images')
     bio = models.TextField()
 
     def __str__(self):
